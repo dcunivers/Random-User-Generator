bef562f112d449df1d48135f7f469514c3936d3b
dana_c <conlindana@gmail.com>
2023-07-07 19:39:02 -0400

    initial commit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+const randomFolks = document.querySelector(".random-peeps");
+
+const getData = async function () {
+    const userRequest = await fetch("https://randomuser.me/api?results=5"
+    );
+    const data = await userRequest.json();
+    //console.log(data);
+
+    const userResults = data.results;
+
+    displayUsers(userResults);
+};
+getData();
+
+const displayUsers = function(userResults) {
+    randomFolks.innerHTML = "";
+
+    for (const user of userResults) {
+        const country = user.location.country;
+        const name = user.name.first;
+        const imageURL = user.picture.medium;
+
+        const userDiv = document.createElement("div");
+         userDiv.innerHTML = `
+                <h3>${name}</h3>
+                <p>${country}</p>
+                <img src=${imageURL} alt="User avatar" />
+            `;
+        randomFolks.append(userDiv);
+    }
+};
+
